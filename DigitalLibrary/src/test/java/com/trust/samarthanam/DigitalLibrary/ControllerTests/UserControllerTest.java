7e88fe24eac079ccb504e7241905ec91803b8f67
unknown <ankithac123@gmail.com>
2020-08-20 12:12:21 +0530

    user controller test done

--- DigitalLibrary/src/test/java/com/trust/samarthanam/DigitalLibrary/ControllerTests/UserControllerTest.java
+++ DigitalLibrary/src/test/java/com/trust/samarthanam/DigitalLibrary/ControllerTests/UserControllerTest.java
@@ -1,4 +1,4 @@
-package com.trust.samarthanam.DigitalLibrary;
+package com.trust.samarthanam.DigitalLibrary.ControllerTests;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
